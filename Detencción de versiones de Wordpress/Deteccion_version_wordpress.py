14b45d3f8d375521cf1c289c51bbbd462ec508c4
Daniel <67976795+Celot1979@users.noreply.github.com>
2022-12-28 18:17:28 +0100

    Primer cambio

---
+++ Detencción de versiones de Wordpress/Deteccion_version_wordpress.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import requests
+from bs4 import BeautifulSoup
+def peticion(direccion):
+    url = direccion
+    cabecera = {'User-Agent':'Safari'}
+    peticion = requests.get(url = url, headers=cabecera)
+    print(peticion.text)
+def main():
+    direccion = input('Dirección de wordpress   ==>  ')
+    peticion(direccion)
+    print("")
+    
+   
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("saliendo")
+        exit()
